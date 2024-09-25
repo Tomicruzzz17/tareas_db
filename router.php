bc248280f2fe2011770a40a2213f07ab4cafcff9
Tomas Cruz <tc22142@gmail.com>
2024-09-25 13:30:40 -0300

    25/09 lista de tareas

---
+++ router.php
@@ -0,0 +1,28 @@
+<?php
+
+
+define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
+
+//**
+//    TABLA DE RUTEO
+//    Action        Funcion
+//    home          showNoticias()
+//    noticia/:id   showNoticia($id) 
+//    about         showAbout()
+//    about/:dev    showAbout($dev)
+
+if (!empty($_GET["action"])){
+    $action = $_GET["action"];
+} else {
+    $action = "home";
+}
+// noticia/3 = ["noticia","3"];
+$params = explode("/",$action);
+
+switch ($params[0]) {
+ 
+    default:
+        echo "Error 404";
+        break;
+}
+
